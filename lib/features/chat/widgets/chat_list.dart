c268f978ad887e665fddb2dcd79ff4168ef986a7
ROG-stdioh <surya.iostream@gmail.com>
2022-11-08 18:53:39 +0530

    automatic scroll done

--- lib/features/chat/widgets/chat_list.dart
+++ lib/features/chat/widgets/chat_list.dart
@@ -5,43 +5,64 @@ import 'package:chat_app_riverpod/models/message.dart';
 import 'package:chat_app_riverpod/widgets/my_message_card.dart';
 import 'package:chat_app_riverpod/widgets/sender_message_card.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class ChatList extends ConsumerWidget {
+class ChatList extends ConsumerStatefulWidget {
   final String recieverUserId;
   const ChatList({required this.recieverUserId, super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _ChatListState();
+}
+
+class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController messageController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    messageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
-        stream: ref.read(chatControllerProvider).chatStream(recieverUserId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Loader();
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              final messageData = snapshot.data![index];
-              var timeSent = DateFormat.Hm().format(messageData.timeSent);
-              if (messageData.senderId ==
-                  FirebaseAuth.instance.currentUser!.uid) {
-                // My Message Card
-                return MyMessageCard(
-                  message: messageData.text,
-                  date: timeSent,
-                );
-              }
-              // Senders Card
-              return SenderMessageCard(
+      stream:
+          ref.read(chatControllerProvider).chatStream(widget.recieverUserId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Loader();
+        }
+
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          messageController.jumpTo(messageController.position.maxScrollExtent);
+        });
+
+        return ListView.builder(
+          controller: messageController,
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            final messageData = snapshot.data![index];
+            var timeSent = DateFormat.Hm().format(messageData.timeSent);
+            if (messageData.senderId ==
+                FirebaseAuth.instance.currentUser!.uid) {
+              // My Message Card
+              return MyMessageCard(
                 message: messageData.text,
                 date: timeSent,
               );
-            },
-          );
-        });
+            }
+            // Senders Card
+            return SenderMessageCard(
+              message: messageData.text,
+              date: timeSent,
+            );
+          },
+        );
+      },
+    );
   }
 }
